937e9083c9e70b775a42ae72e9f0cb9819267da5
Piyush Dutta <piyushdutta0007@gmail.com>
2026-01-31 11:30:45 +0530

    Edited with some changes

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ url = input("Enter a Test or URL : ")
 
 img = qrcode.make(url)
 
-if (int(input("Do you want a Default Filename or a Custom one \n Press 1 for Default Filename \n Press 2 for Custom Filename \n Enter your Choice : ")) == 1):
+n = int(input("Do you want a Default Filename or a Custom one \n Press 1 for Default Filename \n Press 2 for Custom Filename \n Enter your Choice : "))
+
+if (n == 1):
     img.save("QRcode.png")
     print("QR code Generated Successfully")
     print("You will find the QRcode attached in the same folder")
 
-elif (int(input("Do you want a Default Filename or a Custom one \n Press 1 for Default Filename \n Press 2 for Custom Filename \n Enter your Choice : ")) == 2):
+elif (n == 2):
     img.save((input("Enter a custom filename without giving any extension : ") + ".png"))
     print("QR code Generated Successfully")
     print("You will find the QRcode attached in the same folder")
